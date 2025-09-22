ef4fb21c16beacb9302be0308da1725d016c44da
petarcorluka86 <petarcorluka86@gmail.com>
2025-09-22 15:46:41 +0200

    next-hot-reload

--- nextjs.mk
+++ nextjs.mk
@@ -5,14 +5,33 @@
 next-build:
 	docker build -t $(NEXT_IMAGE) $(NEXT_DIR)
 
-next-start: next-build network
+next-build-dev: 
+	docker build -t $(NEXT_IMAGE) -f $(NEXT_DIR)/Dockerfile.dev $(NEXT_DIR)
+
+next-silent-cleanup:
 	-@docker stop $(NEXT_CONTAINER) >/dev/null 2>&1 || true
 	-@docker rm $(NEXT_CONTAINER) >/dev/null 2>&1 || true
+
+next-run: next-silent-cleanup
 	docker run -d \
-		--name $(NEXT_CONTAINER) \
-		--network $(NETWORK) \
-		-p $(NEXT_PORT):$(NEXT_CONTAINER_PORT) \
-		$(NEXT_IMAGE)
+	--name $(NEXT_CONTAINER) \
+	--network $(NETWORK) \
+	-p $(NEXT_PORT):$(NEXT_CONTAINER_PORT) \
+	$(NEXT_IMAGE)
+
+next-run-dev: next-silent-cleanup
+	docker run --rm -it \
+	--name $(NEXT_CONTAINER) \
+	--network $(NETWORK) \
+	-p $(NEXT_PORT):$(NEXT_CONTAINER_PORT) \
+	-v "$(NEXT_DIR):/app" \
+	$(NEXT_IMAGE)
+
+next-start: next-build network next-run
+	@echo "Started Next.js server"
+
+next-start-dev: next-build-dev next-run-dev
+	@echo "Started Next.js server in development mode"
 
 next-stop:
 	-@docker stop $(NEXT_CONTAINER) || true
